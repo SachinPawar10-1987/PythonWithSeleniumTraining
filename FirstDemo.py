31144f6934564b0104cd8926472d037289d5f371
SachinPawar <fairandsqaure.sp@gmail.com>
2024-08-02 02:45:47 -0800

    FirstDemo

---
+++ FirstDemo.py
@@ -0,0 +1,19 @@
+print("Hello Sachin! Welcome to python training.")
+
+# Here is the comments, I have defined.
+
+a = 3
+print(a)
+
+Str = "Hello World"
+print(Str)
+
+b, c, d = 5, 6.4, "Great"
+print(b)
+print(c)
+print(d)
+
+#print("Value is "+b)
+
+"{} {}".format("Value is", b)
+
